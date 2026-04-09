474a2cfefafeab88686279e9afe2efc690f02265
vikas kumar <vk8756008@gmail.com>
2026-04-09 14:29:34 +0530

    deployed

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,17 +18,13 @@ app.use(
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
-// --- STATIC FILES FOR DEPLOYMENT ---
 if (process.env.NODE_ENV === "production") {
-  // 1. Static files serve karein (frontend folder se)
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  // 2. Kisi bhi non-API route par index.html bhejien
-  app.get("(.*)", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-// -----------------------------------
 
 app.listen(PORT, () => {
   console.log("server is running on port:" + PORT);
