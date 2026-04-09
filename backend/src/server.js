0dc78c999b07f66fea965cdfe9496b1bc5957c97
vikas kumar <vk8756008@gmail.com>
2026-04-09 14:13:47 +0530

    deployed

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,9 +24,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/dist")));
 
   // 2. Kisi bhi non-API route par index.html bhejien
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  });
+  app.get("(.*)", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 }
 // -----------------------------------
 
